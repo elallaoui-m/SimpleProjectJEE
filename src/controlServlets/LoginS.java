1aa2b94b66e14cf9d24afc0726d5f3376f4474e9
El allaoui El mehdi <elmehdi.elallaoui@edu.uiz.ac.ma>
2019-01-05 00:11:56 +0000

    b

--- src/controlServlets/LoginS.java
+++ src/controlServlets/LoginS.java
@@ -50,10 +50,15 @@ public class LoginS extends HttpServlet {
 		{
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 			request.getSession().setAttribute("user",Us);
 			request.setAttribute("users",Us);
 			//request.getRequestDispatcher("editProfile.jsp").forward(request, response);
 			//response.getWriter().append("<a href='"+"Login.jsp?logout=1"+"'>Go Backas</a>");
+=======
+			request.getSession().setAttribute("user", Us);
+			response.getWriter().append("<a href='"+"Login.jsp?logout=1"+"'>Go Backas</a>");
+>>>>>>> parent of ae3f0f4... add
 =======
 			request.getSession().setAttribute("user", Us);
 			response.getWriter().append("<a href='"+"Login.jsp?logout=1"+"'>Go Backas</a>");
